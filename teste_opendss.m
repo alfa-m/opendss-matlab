b447297b59fd5630d57ccae9bbff5f1bf64cbdb3
Alfa Marine <alfamarine.ferreira@gmail.com>
2024-10-04 11:58:47 -0300

    realiza correção para a forma de análise harmônica normal

--- teste_opendss.m
+++ teste_opendss.m
@@ -49,64 +49,78 @@ NomesMonitores = string(Monitores.AllNames);
 
 % Define o espectro de frequencias a serem analisadas
 harmonicos = 1:2:25;
+harmonicos = harmonicos';
+mag_harmonicos = 100 * ones(length(harmonicos),1);
+fase_harmonicos = zeros(length(harmonicos),1);
+espectro_harmonico = [harmonicos mag_harmonicos fase_harmonicos];
+writematrix(espectro_harmonico,'espectro_harmonico.csv');
+comando = string(strcat('New spectrum.espectroharmonico numharm=',string(length(harmonicos)),' csvfile=espectro_harmonico.csv'));
+disp(comando);
+DSSText.Command = comando;
 
 % Adiciona a fonte de corrente harmonica de sequencia positiva 
-barra = NomesBarras(7);
+barra = NomesBarras(25);
+comando = string(strcat('New Isource.scansource bus1=',barra,' amps=1 spectrum=espectroharmonico'));
+disp(comando);
+DSSText.Command = comando;
     
 % Realiza fluxo de potencia considerando a fonte de corrente
 DSSSolution.Solve;
 
-% Salva Y
+% Cria matriz Y
 Y = [];
 
-% Salva tensoes
+% Cria matrix tensoes
 V_nodais = [];
 
 % Seleciona o modo harmonico
 DSSText.Command = ['Set mode=harmonic'];
 
-for j = 1:2:25
-    disp(j);
-    comando = string(strcat('Set harmonics=[',string(j),']'));
-    disp(comando);
-    DSSText.Command = comando;
-    DSSSolution.Solve;
-    
-    mySysY = DSSCircuit.SystemY;
-    NomesNos = DSSCircuit.AllNodeNames;
-    TamanhoY = size(NomesNos);
+DSSSolution.Solve;
 
-    myYMat = [];
-    myIdx = 1;
+% for j = 1:2:25
+%     disp(j);
+%     comando = strcat('Set harmonics=(',string(j),')');
+%     disp(comando);
+%     DSSText.Command = comando;
+%     DSSSolution.Solve;
+%     
+%     mySysY = DSSCircuit.SystemY;
+%     NomesNos = DSSCircuit.AllNodeNames;
+%     TamanhoY = size(NomesNos);
+% 
+%     myYMat = [];
+%     myIdx = 1;
+% 
+%     for a = 1:TamanhoY(1)
+%         myRow = [];
+%         for b = 1:TamanhoY(1)
+%             myRow = [myRow,(mySysY(myIdx) + i*mySysY(myIdx + 1))];
+%             myIdx = myIdx + 2;
+%         end;
+%         myYMat = [myYMat;myRow];
+%     end;
+%     
+%     Y = [Y; myYMat];
+%     V_nodais = [V_nodais; DSSCircuit.AllBusVmag];
+% 
+%     % pega amostra dos valores nos monitores
+%     Monitores.SampleAll();
+%     
+%     % salva os valores nos monitores
+%     Monitores.SaveAll();
+% end;
 
-    for a = 1:TamanhoY(1)
-        myRow = [];
-        for b = 1:TamanhoY(1)
-            myRow = [myRow,(mySysY(myIdx) + i*mySysY(myIdx + 1))];
-            myIdx = myIdx + 2;
-        end;
-        myYMat = [myYMat;myRow];
-    end;
-    
-    Y = [Y; myYMat];
-    V_nodais = [V_nodais; DSSCircuit.AllBusVmag];
+DSSText.Command = ['Export monitors all'];
 
-    % samples the active monitor
-    Monitores.SampleAll();
-    % saves the active monitor's registers
-    Monitores.SaveAll();
-end;
-
-%DSSText.Command = ['Export monitors all'];
-
-%%%%%%%%%%%%%%  revisar aqui %%%%%%%%%%%%%%%%%%%
-monitor = NomesMonitores(9);
+%%%%%%%%%%%%%%%  revisar aqui %%%%%%%%%%%%%%%%%%%
+monitor = NomesMonitores(22);
 comando = string(strcat('Plot monitor object=',monitor,' channels=(1 3 5 )'));
 disp(comando);
 DSSText.Command = comando;
-comando = string('Export monitors all');
-disp(comando);
-DSSText.Command = comando;
+% comando = string('Export monitors all');
+% disp(comando);
+% DSSText.Command = comando;
 
 % for k = 1:length(NomesMonitores)
 %     monitor = NomesMonitores(k);
